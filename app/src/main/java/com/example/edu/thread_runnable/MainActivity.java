7d1225c50120b6545fa2f8337e3ef937d18871fc
in6802 <in6802@gmail.com>
2018-11-15 11:39:01 +0900

    design

--- app/src/main/java/com/example/edu/thread_runnable/MainActivity.java
+++ app/src/main/java/com/example/edu/thread_runnable/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.edu.thread_runnable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ProgressBar progressBar;
+    Button buttonRun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBarPost);
+        buttonRun = findViewById(R.id.buttonRun);
+        buttonRun.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
     }
 }
