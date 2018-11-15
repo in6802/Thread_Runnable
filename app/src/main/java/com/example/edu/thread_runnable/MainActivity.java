e858ec53743594b58c8f55b1596934a78b960fd7
in6802 <in6802@gmail.com>
2018-11-15 17:24:44 +0900

    logo image Thread

--- app/src/main/java/com/example/edu/thread_runnable/MainActivity.java
+++ app/src/main/java/com/example/edu/thread_runnable/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.edu.thread_runnable;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.view.View;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -13,12 +23,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button buttonRun;
     private int progressBarStatus = 0;
     TextView textViewProgress;
+    ImageView imageViewPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageViewPost = findViewById(R.id.imageViewLogo);
         progressBarPost = findViewById(R.id.progressBarPost);
         textViewProgress = findViewById(R.id.textViewProgress);
         buttonRun = findViewById(R.id.buttonRun);
@@ -27,9 +39,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        new Thread(new Runnable(){//Runnable을 Thread로 감싸줌. 큐에 올리기 위해서
+        new Thread(new Runnable() {
+            //new Thread(new Runnable(){ Runnable을 Thread로 감싸줌. 큐에 올리기 위해서
             @Override
             public void run() {
+                //logo image
+                String strUri = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
+
+                InputStream inputStream = null;
+                try {
+                    URL url = new URL(strUri);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.connect();
+                    inputStream = httpURLConnection.getInputStream();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+                imageViewPost.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageViewPost.setImageBitmap(bitmap);
+                    }
+                });
+
+                //progress bar
                 while (progressBarStatus < 100) {
                     try {
                         Thread.sleep(1000);//시간을 주는 것
@@ -38,7 +75,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
 
                     progressBarStatus++;
-                    progressBarPost.post(new Runnable() {//post()는 runnuble을 큐에 싣고
+                    progressBarPost.post(new Runnable() {
+                        //post()는 runnuble을 큐에 보냄
                         @Override
                         public void run() { // run() 동작명세서를 만들어줌
                             progressBarPost.setProgress(progressBarStatus);
@@ -47,6 +85,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     });
                 }
             }
-        }).start();//Thread를 동작시키기 위해서 꼭해줘야함.
+        }).start();
+        //start() : Thread를 동작시키기 위해서 꼭해줘야함. ********************************
     }
 }
